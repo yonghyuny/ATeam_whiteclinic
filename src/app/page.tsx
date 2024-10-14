5591568e2edfa0c2be95c09503c7070dc522d65c
yonghyuny <lovelove4695@naver.com>
2024-10-14 18:35:30 +0900

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@
 import * as React from 'react';
 import { Box } from '@mui/material';
 import ACalendar from '@/components/atom/Calendar/ACalendar';
+import TestButton from '@/components/atom/Button/TestButton';
 
 const Home = () => {
-  return <ACalendar></ACalendar>;
+  return (
+    <>
+      <ACalendar></ACalendar>
+      <TestButton />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/atom/Button/TestButton.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@/components/ui/button';
+
+const TestButton = () => {
+  return <Button>button</Button>;
+};
+
+export default TestButton;
