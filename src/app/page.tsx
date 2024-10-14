cac70504bc120061ba0ca51d63533aa92d24426e
yonghyuny <lovelove4695@naver.com>
2024-10-14 18:47:23 +0900

    용현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,11 @@
 import * as React from 'react';
 import { Box } from '@mui/material';
 import ACalendar from '@/components/atom/Calendar/ACalendar';
-import TestButton from '@/components/atom/Button/TestButton';
 
 const Home = () => {
   return (
     <>
       <ACalendar></ACalendar>
-      <TestButton />
     </>
   );
 };
--- src/components/atom/Button/TestButton.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from '@/components/ui/button';
-
-const TestButton = () => {
-  return <Button>button</Button>;
-};
-
-export default TestButton;
